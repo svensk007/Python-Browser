e4f8ce721eeef08686fc2a48c82ebbd9d2e9e879
svensk007 <83252177+svensk007@users.noreply.github.com>
2021-08-19 21:48:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 import sys
+from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
+from PyQt5.QtWebEngineWidgets import *
 
+print("ERR LOG:")
 
 class MainWindow(QMainWindow):
 	def __init__(self):
 		super(MainWindow,self).__init__()
+		self.browser = QWebEngineView()
+		self.browser.setUrl(QUrl('http://google.com'))
+		self.setCentralWidget(self.browser)
 		self.showMaximized()
+
+# NAVBAR
+navbar = QToolBar()
+self.addToolBar(navbar)
+
+back_btn = QAction('Back',self)
+back_btn.triggered.connect(self.browser.back)
 
 app = QApplication(sys.argv)
 QApplication.setApplicationName('Browser')
